076848a875ae476075a7d131e7107ddc110118c0
Mansi Joshi <mjmansi2620@gmail.com>
2026-02-16 12:44:23 +0530

    Time: 55 ms (28.95%) | Memory: 57.9 MB (16.99%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -3,27 +3,23 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    let count =init;
+      var counter =init
     return {
-        increment : function (){
-            count++;
-           return count;
+        increment : function(){
+            return ++counter
         },
-        decrement : function (){
-            count--;
-           return count;
+        decrement : function(){
+            return --counter
         },
-        reset : function(){
-           count=init;
-            return count;
+        reset :function(){
+            counter=init
+            return counter
         }
     }
-    
 };
 
 
   const counter = createCounter(5)
- counter.increment(); // 6
- counter.reset(); // 5
+  counter.increment(); // 6
+  counter.reset(); // 5
  counter.decrement(); // 4
- 
