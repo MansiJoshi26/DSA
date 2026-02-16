68f7902a33ab7a4cd6134e14537b32ad4b7d6c8b
Mansi Joshi <mjmansi2620@gmail.com>
2026-02-16 12:15:17 +0530

    Time: 42 ms (56.86%) | Memory: 53.9 MB (35.87%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -1,14 +1,12 @@
-/**
- * @return {Function}
- */
+
 var createHelloWorld = function() {
     
-    return function(...args) {
-        return "Hello World";
+    return function() {
+            return "Hello World"
+        
     }
 };
 
 
-  const f = createHelloWorld();
-  f(); // "Hello World"
- 
+ const f = createHelloWorld();
+ f(); // "Hello World"
